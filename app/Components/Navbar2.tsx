ac7612b7a43f5359608e4ff5368844bf6de2e04f
Oluwaseyi Tommy <130176232+seshconcept@users.noreply.github.com>
2024-07-09 09:40:38 +0100

    update

--- app/Components/Navbar2.tsx
+++ app/Components/Navbar2.tsx
@@ -19,7 +19,7 @@ const Navbar2 = () => {
     <div className="flex items-center justify-between w-[100%] h-auto bg-white text-black py-5 px-6 md:px-10 fixed top-0 z-20 space-y-2">
       <p
         className={`w-1/3  leading-7 font-semibold  text-[#2C6A2F]
-           md:text-[28px] italic ${playfair.className}`}
+        text-xl md:text-2xl italic ${playfair.className}`}
       >
         Food Court
       </p>
@@ -53,7 +53,7 @@ const Navbar2 = () => {
               {/* <button className=" w-auto h-12 px-4 py-1 md:py-2 me-2  bg-white border-2 border-gray-500 rounded-3xl hover:bg-[#4CAF50]   hover:text-white hover:border-0"> */}
               <div className="text-xl">
                 {" "}
-                <FaCartShopping />{" "}
+                <FaCartShopping />{""}
               </div>
               <div className="">{cart.length}</div>
             </Link>
@@ -62,9 +62,9 @@ const Navbar2 = () => {
       </div>
 
       {nav && (
-        <div className="md:hidden justify-between items-center gap-8 absolute  top-22 lg:top-14 left-0 bg-gray-900 w-full h-60 p-6 space-y-6 text-lg text-white">
+       <div className="md:hidden justify-between items-center gap-8 absolute  top-[88px] lg:top-14 left-0 bg-gray-900  w-[70%] h-96 p-6 space-y-6 text-lg text-white">
           <ul>
-            <div className="space-y-20 gap-6 ">
+            <div className="space-y-20 gap-6">
               <Link href="/">
                 <li className="hover:text-[#4CAF50]  cursor-pointer">Home </li>
               </Link>
@@ -93,7 +93,7 @@ const Navbar2 = () => {
 
       {!nav ? (
         <div className=" md:hidden cursor-pointer" onClick={() => setNav(true)}>
-          <GiHamburgerMenu size={30} />
+          <GiHamburgerMenu size={28} />
         </div>
       ) : (
         <div
