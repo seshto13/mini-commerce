5e9fc9206102456b0eaa1107309b4427947bd705
Oluwaseyi Tommy <130176232+seshconcept@users.noreply.github.com>
2024-07-11 22:01:06 +0100

    update

--- app/Components/Landing.tsx
+++ app/Components/Landing.tsx
@@ -9,12 +9,16 @@ const playfair = Playfair_Display({ subsets: ["latin"] });
 const Landing = () => {
   return (
     <div className="w-full h-screen px-4 my-4">
-      <div className="w-full h-screen bg-[url('/assets/images/image11.jpg')] bg-cover bg-center flex items-center  justify-center bg-transparent bg-opacity">
+      <div className="w-full h-screen bg-[url('/assets/images/bgimage11.jpg')] bg-cover bg-center flex items-center  justify-center bg-transparent bg-opacity">
         <div className=" text-center ">
-         <Bounce> <p className={`text-[50px] lg:px-52 font-medium ${playfair.className}`}>
-            Get Your Favourite <br />
-            Meals Delivered
-          </p>
+          <Bounce>
+            {" "}
+            <p
+              className={`text-[50px] lg:px-52 font-medium ${playfair.className}`}
+            >
+              Get Your Favourite <br />
+              Meals Delivered
+            </p>
           </Bounce>
           <br />
           <p className="text-[20px]">
@@ -22,7 +26,7 @@ const Landing = () => {
             welcoming, culinary haven.
           </p>
           <br />
-          
+
           <div className=" flex items-center justify-center md:gap-10">
             <a href="/menu" className=" w-auto  ">
               <button className="w-36 md:w-[170px] h-14 md:h-16 border-[1.5px] px-6 py-2 me-2  bg-[#4CAF50] text-white hover:bg-lime-200 hover:text-[bg-[#4CAF50] border-white rounded-full">
@@ -31,8 +35,10 @@ const Landing = () => {
             </a>
 
             <a href="/buy" className="w-auto">
-              <button className="w-36 md:w-[170px] h-14 md:h-16 px-6 py-2 me-2  bg-transparent  hover:bg-lime-200  hover:text-bg-[#4CAF50]
-border-[1.5px]  border-black hover:border-white rounded-full">
+              <button
+                className="w-36 md:w-[170px] h-14 md:h-16 px-6 py-2 me-2  bg-transparent  hover:bg-lime-200  hover:text-bg-[#4CAF50]
+border-[1.5px]  border-black hover:border-white rounded-full"
+              >
                 More Menu
               </button>
             </a>
